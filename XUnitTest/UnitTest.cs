a9677bb68de46df2980d917f6eabd5c1a8c2186e
yuruisoft <417853832@qq.com>
2018-08-17 13:12:36 +0800

    ...

--- XUnitTest/UnitTest.cs
+++ XUnitTest/UnitTest.cs
@@ -13,7 +13,7 @@ namespace XUnitTest
         public static string dbString = "server=127.0.0.1;user=root;password=123456;port=3306;IgnorePrepare=false;Pooling=false;";
         [Fact]
         public void Test1()
-        {
+        {      
             dbSvc.Start(dbString, 32);
             workerMgr.Start(32);
         }
